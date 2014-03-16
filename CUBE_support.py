581eeb61044e7a8d2eb57b2c8f2a45e236343737
Joseph Leeper <jl933a@lab.icc.edu>
2014-03-15 23:33:14 -0500

    Convoluted but necessary testing.

--- CUBE_support.py
+++ CUBE_support.py
@@ -2,14 +2,14 @@
 from binascii import crc32
 import struct
 
-#def output_object(layer, input_pattern):
-#    return (layer, input_pattern)
 
 def output_number(input_object):
     return (input_object[0] << 16) | input_object[1]
 
+
 def output_string(input_number):
     return "{0:032b}".format(input_number)
+
 
 def output_bytes(input_object):
     in_data_layer = struct.pack('>B', input_object[0])
@@ -21,10 +21,15 @@ def output_bytes(input_object):
     return in_data_layer + in_data_packed + in_data_checked
 
 
+def check_output_bytes(input_bytes, test_layer, test_pattern, test_crc):
+    in_data_unpacked = struct.unpack('>BHL', input_bytes)
+    return (in_data_unpacked[0] == test_layer, in_data_unpacked[1] == test_pattern, in_data_unpacked[2] == test_crc)
+
+
 if __name__ == '__main__':
 
     test_layer = 2
-    test_pattern = 0x4321
+    test_pattern = 0x4321 
 
     test_object = (test_layer, test_pattern)
     test_number = output_number(test_object)
@@ -33,5 +38,8 @@ if __name__ == '__main__':
     print(test_object)
     print(test_number)
     print(output_string(test_number))
-    print("{0:032b}".format(crc32(struct.pack('>L', test_number))))
-    print(output_bytes(test_object))
+    test_crc = crc32(struct.pack('>L', test_number))
+    print("{0:032b}".format(test_crc))
+    test_output_bytes = output_bytes(test_object)
+    print(test_output_bytes)
+    print(check_output_bytes(test_output_bytes, test_layer, test_pattern, test_crc))
